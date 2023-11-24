e4bda28206a40935bd04679d242d45d99cf672ad
KingofTime <antonio_neto77@hotmail.com.br>
2023-11-24 03:38:01 -0300

    Feat: Adds internationalization to responses

---
+++ lang/en/response.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Response Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used when returning requests
+    | You are free to modify these lines of language according to
+    | your application requirements.
+    |
+    */
+
+    'store' => ':Resource created successfully',
+    'update' => ':Resource edited successfully',
+    'destroy' => ':Resource successfully removed',
+];
